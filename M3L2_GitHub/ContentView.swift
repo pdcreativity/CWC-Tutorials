9ce67bb898a4bdc8337d343e11b8f35d31ccd101
Paul Damario <paul@pdcreativity.com>
2022-01-02 13:11:47 -0500

    Initial Commit

---
+++ M3L2_GitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M3L2_GitHub
+//
+//  Created by Paul Damario on 2022-01-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M3L2_GitHub/M3L2_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  M3L2_GitHubApp.swift
+//  M3L2_GitHub
+//
+//  Created by Paul Damario on 2022-01-02.
+//
+
+import SwiftUI
+
+@main
+struct M3L2_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
